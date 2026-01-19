d5a2e4e1201ee9767a24c788163d7025150a4098
Exda27 <93272461+Exda27@users.noreply.github.com>
2026-01-19 03:25:19 -0500

    Revert "Switch Node.js server to Bun"

--- start.sh
+++ start.sh
@@ -3,14 +3,14 @@
 # Make sure pwd is the directory of the script
 cd "$(dirname "$0")"
 
-if ! command -v bun &> /dev/null
+if ! command -v npm &> /dev/null
 then
-    echo -e "\033[0;31mbun could not be found in PATH. If the startup fails, please install bun from https://bun.sh/\033[0m"
+    echo -e "\033[0;31mnpm could not be found in PATH. If the startup fails, please install Node.js from https://nodejs.org/\033[0m"
 fi
 
-echo "Installing Bun Modules..."
+echo "Installing Node Modules..."
 export NODE_ENV=production
-bun i --no-save --no-audit --no-fund --loglevel=error --no-progress --omit=dev
+npm i --no-save --no-audit --no-fund --loglevel=error --no-progress --omit=dev
 
 echo "Entering SillyTavern..."
-bun "server.js" "$@"
+node "server.js" "$@"
